33e7c8587132b07c4a83d0a9f44ad73a339391a5
Gegham Zakaryan <zakaryan.2004@outlook.com>
2019-07-01 15:32:20 +0400

    msm8916-common: Welcome to Evolution X!
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,9 +96,9 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
 # CMHW
 #BOARD_USES_CYANOGEN_HARDWARE := true
-JAVA_SOURCE_OVERLAYS += \
-	org.lineageos.hardware|hardware/samsung/lineagehw|**/*.java \
-	org.lineageos.hardware|$(LOCAL_PATH)/lineagehw|**/*.java
+# JAVA_SOURCE_OVERLAYS += \
+# 	org.lineageos.hardware|hardware/samsung/lineagehw|**/*.java \
+# 	org.lineageos.hardware|$(LOCAL_PATH)/lineagehw|**/*.java
 
 # Display
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
